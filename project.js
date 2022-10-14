df674632087e1c5c901ee4b69a50fbf0122e8990
Shivam Singh 520 <78562652+shivam12-cs@users.noreply.github.com>
2022-10-14 21:57:30 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Data Scientist", "Software-Developer", "BasketBall Player", "fullstack-developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings:  ["Data Scientist", "Software-Developer", "fullstack-developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
